88f00b682aa12658ecc23b31a4ce4c45c3768943
TeNiReY <tenirey11@gmail.com>
2025-04-23 18:17:16 +0300

    Initial Commit

---
+++ SocialNetworkService/src/main/java/com/socialnetworkservice/socialnetworkservice/SocialNetworkServiceApplication.java
@@ -0,0 +1,13 @@
+package com.socialnetworkservice.socialnetworkservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialNetworkServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialNetworkServiceApplication.class, args);
+    }
+
+}

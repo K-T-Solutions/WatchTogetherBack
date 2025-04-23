d94652c4d64a853c71eb0319d2f5130a1ac90e6b
alex <kartoxa.grozney@gmail.com>
2025-04-23 11:30:26 +0300

    add User class

---
+++ authorization/src/main/java/com/KAndT/authorization/User.java
@@ -0,0 +1,25 @@
+package com.KAndT.authorization;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private int role;
+}
